17735c68d0e68a17661b275bae14ffa641235ce8
ali <ali@MacBook-Pro-Ali.local>
2023-01-03 03:18:18 +0400

    mesaj

---
+++ alqoritm suali 10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace alqoritm_suali_10;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int min = 0;
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < numbers[min])
+                min = i;
+
+        }Console.WriteLine(min);
+
+
+    }
+}
+
